f9f6f8f19d16d2d8f7de2b36c34d05c60589589d
Sandeep-Tech <sandeeptech8@gmail.com>
2021-01-13 16:01:02 +0530

    added elements to the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,26 @@
 import React from "react";
 import style from "./Header.module.css";
 
-const Header = () => {
-  return <div className={style.header} >Header</div>;
-};
+const Header = () => (
+  <div className={style.header}>
+    {/*logo*/}
+    <span>Sandeeplex</span>
+
+    {/*content type list*/}
+    <div>
+      <a>Originals</a>
+      <a>Movies</a>
+      <a>Videos</a>
+      <a>Music</a>
+      <a>Hype</a>
+    </div>
+
+    {/*theme selector*/}
+    <div> light or dark</div>
+
+    {/*login-button(a span tag really)*/}
+    <span>Login</span>
+  </div>
+);
 
 export default Header;
