a49de04d82d6efbca0ee755d97203ece3050d7b5
Sandeep-Tech <sandeeptech8@gmail.com>
2021-01-14 12:38:11 +0530

    added footer and got the logo

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        <a href="">Subscription</a>
+        <a href="">Hype</a>
+        <a href="">About Us</a>
+        <a href="">Faqs</a>
+        <a href="">Career</a>
+        <a href="">Feedback</a>
+        <a href="">Support</a>
+        <a href="">Contact Us</a>
+      </div>
+
+      <div>
+        <span>Available on</span>
+      </div>
+
+      <div>
+        <span>Connect With Us</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
